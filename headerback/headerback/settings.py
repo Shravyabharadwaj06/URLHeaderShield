0ec3f6b9826e16e7b2d050a77877903019fcada4
Shravyabharadwaj06 <shravyabharadwaj3176@gmail.com>
2026-04-02 20:37:02 +0530

    Prepare for production deployment on Render/Vercel

--- headerback/headerback/settings.py
+++ headerback/headerback/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%4o1(s!ta%o&84wdw@8@rhm=tij8%67l)zs=fgmp&wkn&4v6u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4%4o1(s!ta%o&84wdw@8@rhm=tij8%67l)zs=fgmp&wkn&4v6u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allows Render and localhost
+
 
 
 # Application definition
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# For whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
@@ -127,6 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'headeranalyzergsss@gmail.com'
-EMAIL_HOST_PASSWORD = 'drqv pjpm dgbx llbq'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'headeranalyzergsss@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'drqv pjpm dgbx llbq')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
